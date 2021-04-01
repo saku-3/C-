f2d0b62e8df676901d2b9f02c389077d190f686a
saku-3 <78265674+saku-3@users.noreply.github.com>
2021-04-01 20:26:01 +0900

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReminderApp_20210328.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
